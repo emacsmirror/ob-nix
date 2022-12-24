442eb04bc1d17f14c96fdecdb2ebe068235183b2
Wilko <w-codeberg@wmeyer.eu>
2022-12-24 03:01:05 +0100

    added: first working version of ob-nix.el

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
 # ob-nix
 
-a simple org-babel language extension to evaluate nix expressions with nix-instantiate
+A simple org-babel language extension to evaluate nix expressions using `nix-instantiate`.
+
+## Background of this Project/Code Quality
+
+I recently started using `NixOS` as well as getting into `nix`. As I heavily rely on `org-mode`/`org-roam` for personal knowledge management and `org-babel` to add literate examples I needed a simple way to evaluate `nix` source-blocks with `org-babel`.
+
+The code was written in one-sitting, I intent to improve `ob-nix` further. I took quite some inspiration from [arnm/ob-mermaid](https://github.com/arnm/ob-mermaid) as well as from the `ob-template.el`, so the code structure of this project is loosely based on the aforementioned projects as this is the first time that I've written `org-babel` support for a programming language.
+
+## Examples
+
+```
+#+begin_src nix
+23+19
+#+end_src
+
+#+RESULTS:
+: 42
+```
+
+### Output-Formats
+
+I've implemented support for =nix-instantiates= JSON as well as XML output format:
+
+```
+--json
+	When used with --eval, print the resulting value as an JSON representation of the abstract syntax tree rather than as an ATerm.
+
+--xml
+	When used with --eval, print the resulting value as an XML representation of the abstract syntax tree rather than as an ATerm.  The schema is the same  as  that
+	used by the toXML built-in <../language/builtins.md>.
+```
+
+#### JSON-Output
+
+```
+#+begin_src nix :json t
+rec { x = "foo"; y = x; }
+#+end_src
+
+#+RESULTS:
+: {"x":"foo","y":"foo"}
+```
+
+#### XML-Output
+
+```
+#+begin_src nix :xml t
+  let
+    delimiter = " ";
+    a = abort "thanks for the fish";
+    b = "hello";
+    c = "world";
+  in b + delimiter + c
+#+end_src
+
+
+#+RESULTS:
+: <?xml version='1.0' encoding='utf-8'?>
+: <expr>
+:   <string value="hello world" />
+: </expr>
+```
+
---
+++ ob-nix.el
@@ -0,0 +1,71 @@
+;;; ob-nix.el --- simple org-babel support for nix
+
+;; Copyright (C) 2022 Wilko Meyer
+
+;; Author: Wilko Meyer <w-devel@wmeyer.eu>
+;; Keywords: literate programming, nix
+;; Homepage: https://codeberg.org/theesm/ob-nix
+;; Version: 0.01
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; Simple org-babel language support for nix expressions
+
+;;; Requirements:
+
+;; nix as well as nix-instantiate has to be installed and available
+
+;;; Code:
+(require 'ob)
+(require 'ob-ref)
+(require 'ob-comint)
+(require 'ob-eval)
+;; possibly require modes required for your language
+
+(defcustom org-babel-nix-command "nix-instantiate"
+  "Name of command to use for executing nix code."
+  :group 'org-babel
+  :type 'string
+  )
+
+(defun org-babel-execute:nix (body params)
+  "Evaluate nix code with org-babel."
+  (let ((in-file (org-babel-temp-file "nix" ".nix"))
+	 (json (cdr (assoc :json params)))
+	 (xml (cdr (assoc :xml params)))
+         (verbosity (or (cdr (assq :verbosity params)) t)))
+    (let ((cmd (concat org-babel-nix-command
+		 " --eval "
+		 (if json
+		 "--json ")
+		 (if xml
+		 "--xml ")
+		 (if verbosity
+		 "--verbose ")
+		 " -- "
+		 (org-babel-process-file-name in-file))))
+    (with-temp-file in-file
+      (insert body))
+    (message "%s" cmd)
+    (org-babel-eval cmd "")
+    )))
+
+(provide 'ob-nix)
+
